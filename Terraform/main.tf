06a9cf1a5322db7f622dc38e46be73743739a0db
shubh dadhich <shubhdadhich81@gmail.com>
2025-04-22 17:41:25 +0530

    modified main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,7 +1,20 @@
-module "provisioner" {
-  source = "./modules/provisioner"
+//module "provisioner" {
+  //source = "./modules/provisioner"
+  //region = var.region
+//}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
   region = var.region
 }
+
 
 module "asg" {
   source = "./modules/asg"
